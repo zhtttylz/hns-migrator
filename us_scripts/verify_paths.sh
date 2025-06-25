dedf4befa89b533c969f27747e2f230c8de75b69
zhtttylz <hualong.z@hotmail.com>
2025-06-25 14:52:54 +0800

    修改部分内容

--- us_scripts/verify_paths.sh
+++ us_scripts/verify_paths.sh
@@ -24,10 +24,32 @@ for ((i=0; i<limit; i++)); do
   group=$(echo "$line" | awk '{print $3}')
   # 这里固定为美东
   target=$(echo "$path" | sed "s/DClusterUS1/DClusterUS2/g")
-
+  echo "===================================================="
   if ! hadoop fs -ls -d "$target" >/dev/null 2>&1; then
     echo "hadoop fs -mkdir $target"
     echo "hadoop fs -chown $owner:$group $target"
   fi
-
 done
+
+last_line=$(tail -n 1 "$input_file")
+path=$(echo "$last_line" | awk '{print $1}')
+owner=$(echo "$last_line" | awk '{print $2}')
+group=$(echo "$last_line" | awk '{print $3}')
+
+target_ns2=$(echo "$path" | sed "s/DClusterUS1/DClusterUS2/g")
+base_dir=$(dirname "$target_ns2")
+table_name=$(basename "$target_ns2")
+ns2_base="$base_dir/ns2"
+ns2_path="$ns2_base/$table_name"
+
+echo "===================================================="
+if ! hadoop fs -ls -d "$ns2_base" >/dev/null 2>&1; then
+  echo "hadoop fs -mkdir $ns2_base"
+  echo "hadoop fs -chown $owner:$group $ns2_base"
+fi
+
+echo "===================================================="
+if ! hadoop fs -ls -d "$ns2_path" >/dev/null 2>&1; then
+  echo "hadoop fs -mkdir $ns2_path"
+  echo "hadoop fs -chown $owner:$group $ns2_path"
+fi
