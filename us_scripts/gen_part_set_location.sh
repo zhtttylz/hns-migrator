140933eed8ec43791c96eb012d7108e6b56a4e56
zhtttylz <hualogzhang@gmail.com>
2025-06-26 17:03:09 +0800

    批量修改hive location

--- us_scripts/gen_part_set_location.sh
+++ us_scripts/gen_part_set_location.sh
@@ -10,28 +10,27 @@ fi
 
 table=$1
 
-db_name=${table%%.*}
-table_name=${table##*.}
-
-# 获取库的location
-DB_LOC=$(spark-sql --master local -e "DESC DATABASE EXTENDED $db_name" 2>/dev/null | grep -i '^Location:' | awk '{print $2}')
-if [ -z "$DB_LOC" ]; then
-  echo "Failed to get location for database $db_name" >&2
+# 获取表的 location
+TABLE_LOC=$(spark-sql --master local -e "DESC FORMATTED $table" 2>/dev/null | grep -i '^Location' | awk '{print $2}')
+if [ -z "$TABLE_LOC" ]; then
+  echo "Failed to get location for table $table" >&2
   exit 1
 fi
 
-# 目标库路径，替换集群名称
-NEW_DB_LOC=${DB_LOC/DClusterUS1/DClusterUS2}
+BASE_DIR=$(dirname "$TABLE_LOC")
+NEW_BASE_DIR=${BASE_DIR/DClusterUS1/DClusterUS2}
 
-# 查询所有分区
-mapfile -t parts < <(spark-sql --master local -e "SHOW PARTITIONS $table" 2>/dev/null)
+# 查询所有分区，将其放入数组，并打印分区数量
+# 去除show partition时，打印的”partition“ 字段
+mapfile -t parts < <(spark-sql --master local -e "SHOW PARTITIONS $table" 2>/dev/null | grep -v -i -x 'partition' )
+echo "总共 ${#parts[@]} 个分区"
 
 for part in "${parts[@]}"; do
   # 将 part 形如 k=v/k2=v2 转为 k=v,k2=v2
   spec=$(echo "$part" | sed 's#/#, #g')
-
-  loc=$(spark-sql --master local -e "DESC FORMATTED $table PARTITION ($spec)" 2>/dev/null | grep -i '^Location:' | awk '{print $2}')
-
+  echo "扫描分区${spec}对应的分区路径"
+  loc=$(spark-sql --master local -e "DESC FORMATTED $table PARTITION ($spec)" 2>/dev/null | grep -i '^Location' | awk '{print $2}')
+  echo "${spec}的分区路径为${loc}"
   if [[ -z "$loc" ]]; then
     echo "Skip partition $part: cannot get location" >&2
     continue
@@ -42,14 +41,12 @@ for part in "${parts[@]}"; do
     continue
   fi
 
-  # 判断库路径是否已经带 ns2
-  if [[ $loc == $DB_LOC/ns2/* ]]; then
-    rest=${loc#"$DB_LOC/"}
+  rest=${loc#"${BASE_DIR}/"}
+  if [[ $rest == ns2/* ]]; then
+    new_loc="$NEW_BASE_DIR/$rest"
   else
-    rest="ns2/${loc#"$DB_LOC/"}"
+    new_loc="$NEW_BASE_DIR/ns2/$rest"
   fi
-
-  new_loc="$NEW_DB_LOC/$rest"
 
   echo "ALTER TABLE $table PARTITION ($spec) SET LOCATION '$new_loc';"
 done
